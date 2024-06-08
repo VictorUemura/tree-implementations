4577c2a97ee2156a3d97c48d52e1ebd680eb6c41
VictorUemura <victoruemura04@hotmail.com>
2024-06-07 21:02:43 -0300

    feat: implementacao da previa da insercao btree

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import b.tree.BTree;
 import n.area.NArea;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void testNAreaTree() {
         NArea nArea = new NArea();
-        for(int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             nArea.insere(i);
         }
 
         nArea.inOrdem();
+    }
+
+    public static void testBTree() {
+        BTree bTree = new BTree();
+        for (int i = 0; i < 100; i++) {
+            bTree.insert(i, i);
+        }
+
+        bTree.inOrdem();
+    }
+
+    public static void main(String[] args) {
+        testBTree();
     }
 }
--- src/b/tree/BTree.java
+++ src/b/tree/BTree.java
@@ -1,5 +1,113 @@
 package b.tree;
 
 public class BTree {
+    private No root;
+
+    public BTree() {
+        root = null;
+    }
+
+    // busca pai para caso seja necessario realizar um split do no da arvore
+    private No buscaPai(No filho) {
+        No aux = root;
+        No pai = aux;
+
+        int pos;
+        int valueAux = filho.getVInfo(0);
+        while (aux != filho) {
+            pos = aux.procurarPosicao(valueAux);
+            pai = aux;
+            aux = pai.getVLig(pos);
+        }
+
+        return pai;
+    }
+
+    private void split(No noExcedente) {
+        No pai;
+        No cx1, cx2;
+        int pos, valorInsercao, posicaoInsercao;
+
+        while (noExcedente.getTl() == No.m * 2 + 1) {
+            pai = buscaPai(noExcedente);
+
+            cx1 = new No();
+            cx2 = new No();
+
+            for (int i = 0; i < No.m; i++) {
+                cx1.setVInfo(i, noExcedente.getVInfo(i));
+                cx1.setVPos(i, noExcedente.getVPos(i));
+                cx1.setTl(cx1.getTl() + 1);
+            }
+
+            for (int i = No.m + 1; i < noExcedente.getTl(); i++) {
+                cx2.setVInfo(i - No.m - 1, noExcedente.getVInfo(i));
+                cx2.setVPos(i - No.m - 1, noExcedente.getVPos(i));
+                cx2.setTl(cx2.getTl() + 1);
+            }
+
+            valorInsercao = noExcedente.getVInfo(No.m);
+            posicaoInsercao = noExcedente.getVPos(No.m);
+
+            if (noExcedente == pai) {
+                noExcedente.setVInfo(0, valorInsercao);
+                noExcedente.setVPos(0, valorInsercao);
+                noExcedente.setVLig(0, cx1);
+                noExcedente.setVLig(1, cx2);
+                noExcedente.setTl(1);
+            } else {
+                pos = pai.procurarPosicao(valorInsercao);
+                pai.remanejar(pos);
+                pai.setVInfo(pos, valorInsercao);
+                pai.setVPos(pos, posicaoInsercao);
+
+                pai.setVLig(pos, cx1);
+                pai.setVLig(pos + 1, cx2);
+                pai.setTl(pai.getTl() + 1);
+            }
+
+            noExcedente = pai;
+        }
+    }
+
+    public void insert(int value, int index) {
+        int pos;
+        No aux;
+
+        // caso se nao existir nenhum elemento dentro da btree
+        // nesse caso o tl - tamanho logico - e incrementado automaticamente
+        if (root == null)
+            root = new No(value, index);
+        else {
+            aux = root;
+            pos = aux.procurarPosicao(value);
+            while (aux.getVLig(0) != null) {
+                aux = aux.getVLig(pos);
+                pos = aux.procurarPosicao(value);
+            }
+
+            aux.remanejar(pos);
+            aux.setVInfo(pos, value);
+            aux.setVPos(pos, index);
+            aux.setTl(aux.getTl() + 1);
+
+            if (aux.getTl() == No.m * 2 + 1)
+                split(aux);
+        }
+    }
+
+    public void inOrdem() {
+        inOrdem(root);
+    }
+
+    public void inOrdem(No aux) {
+        if (aux != null) {
+            for (int i = 0; i < aux.getTl(); i++) {
+                inOrdem(aux.getVLig(i));
+                System.out.println("Valor: " + aux.getVInfo(i) + " - Index: " + aux.getVPos(i));
+            }
+            inOrdem(aux.getVLig(aux.getTl()));
+        }
+    }
 
 }
